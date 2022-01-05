dd345bf55039fff8ebd87314d35c15749d0cc513
Ashish Pandey <96864534+ashishdeopandey@users.noreply.github.com>
2022-01-05 20:29:21 +0530

    Update larg3.java

--- larg3.java
+++ larg3.java
@@ -1,6 +1,22 @@
+import java.util.Scanner; 
 class larg3{
     public static void main(String[] args) {
-     System.out.println("Hello Java"); 
-        System.out.println("Hello Java"); 
+        int a, b, c, larg;  
+        Scanner sc = new Scanner(System.in);  
+        System.out.println("Enter the first no");  
+        a = sc.nextInt();  
+        System.out.println("Enter the second no");  
+        b = sc.nextInt();  
+        System.out.println("Enter the third no");  
+        c = sc.nextInt();  
+        if(a>=b && a>=c)  {
+        System.out.println("The largest Number is " + a);    
+        }
+        else if (b>=a && b>=c)  
+        System.out.println("The largest Number is " + b);    
+        else  
+         {
+        System.out.println("The largest Number is " + c);
+         }
     }
 }
